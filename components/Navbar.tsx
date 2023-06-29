79d2e7cf76b701605a0076b06911995ebda33a73
RawkFoo <rawkfoo@gmail.com>
2023-06-29 15:25:44 -0400

    server connection

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,9 +2,12 @@ import { NavLinks } from '@/constants'
 import Image from 'next/image'
 import Link from 'next/link'
 import AuthProviders from './AuthProviders';
+import { getCurrentUser } from '@/lib/session';
+import { signOut } from 'next-auth/react'
+import ProfileMenu from './ProfileMenu';
 
-const Navbar = () => {
-  const session = {};
+const Navbar = async () => {
+  const session = await getCurrentUser();
 
   return (
     <nav className='flexBetween navbar'>
@@ -28,17 +31,17 @@ const Navbar = () => {
 
         <div className='flexCenter gap-4'>
             
-            {session ? (
+            {session?.user ? (
               <>
-              UserPhoto
-              
-              <Link href="/create-project">
-              Share Work
-              </Link>
+                <ProfileMenu session={session}/>
+
+                <Link href="/create-project">
+                  Share Work
+                </Link>
               </>
-            ) : (
-              <AuthProviders />
-            )}
+              ) : (
+                <AuthProviders />
+              )}
 
         </div>
     </nav>
--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ['lh3.googleusercontent.com']
+  },
+    // if it doesnt work in the future use this slice
+    experimental: {
+      serverComponentsExternalPackages: ['cloudinary', 'graphql-request']
+  },
+}
 
 module.exports = nextConfig
